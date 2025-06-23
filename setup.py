f60df6fddcc90a94d8bd46f1dee7e1bfdf63fb93
Abhishek Mishra <47077729+abhishek17569@users.noreply.github.com>
2025-06-24 01:09:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fastapi_grpc_bridge',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'fastapi',
